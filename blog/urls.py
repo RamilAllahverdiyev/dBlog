8b47260b364e80daf2000ac6e08f3a44a22a914c
Ramil Allahverdiyev <allahverdiyevramil@yahoo.com>
2022-09-09 00:42:02 +0300

    forget password

--- blog/urls.py
+++ blog/urls.py
@@ -25,9 +25,15 @@ urlpatterns = [
     path('register/', views.registerPage, name='blog-register'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='blog-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='blog-logout'),
-    path('profile/', views.profile, name='blog-profile')
+    path('profile/', views.profile, name='blog-profile'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='blog/password_reset.html'), name='password-reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='blog/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
