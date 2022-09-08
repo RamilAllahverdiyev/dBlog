934efda7e630253cc5351aad1a2af93e24493217
Ramil Allahverdiyev <allahverdiyevramil@yahoo.com>
2022-09-09 00:56:10 +0300

    forget password part completed

--- blog/urls.py
+++ blog/urls.py
@@ -30,9 +30,10 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='blog/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
-             template_name='users/password_reset_confirm.html'
+             template_name='blog/password_reset_confirm.html'
          ),
          name='password_reset_confirm'),
+     path('password-reset-complete/', auth_views.PasswordResetView.as_view(template_name='blog/password_reset_complete.html'), name='password_reset_complete'),     
 ]
 
 if settings.DEBUG:
