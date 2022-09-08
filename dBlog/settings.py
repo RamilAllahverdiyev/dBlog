e12600a572644a9d302cd10e2badd160af98abd4
Ramil Allahverdiyev <allahverdiyevramil@yahoo.com>
2022-09-09 01:17:09 +0300

    connected to mysql

--- dBlog/settings.py
+++ dBlog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from re import M
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'dBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_sql',
+        'USER':'root',
+        'PASSWORD':'Valride1@',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
