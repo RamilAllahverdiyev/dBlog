620b965c486d88ec47e011c6a9297e4da8fc0c0f
Ramil Allahverdiyev <allahverdiyevramil@yahoo.com>
2022-09-10 17:35:34 +0300

    fiveee

--- dBlog/settings.py
+++ dBlog/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'dBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_sql',
-        'USER':'root',
-        'PASSWORD':'Valride1@',
-        'HOST':'127.0.0.1',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
