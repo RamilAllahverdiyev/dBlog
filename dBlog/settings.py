45fd25a094a6f2f15e138ee975b804f2052a6727
Ramil Allahverdiyev <allahverdiyevramil@yahoo.com>
2022-09-10 16:54:42 +0300

    deployment

--- dBlog/settings.py
+++ dBlog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+^1#_2v0@zk5jn8@vp!o!sp8ef935)3p0iq6k%#o9p^xm7gh$w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dblogd.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
